cb2b2f19c6496986c7183a019ad24379df09d0cb
Michael250-1 <79066678+Michael250-1@users.noreply.github.com>
2021-06-15 19:21:35 -0700

    Add files via upload

---
+++ game.js
@@ -0,0 +1,7 @@
+function adduser(){
+    player1=document.getElementById("player1").Value;
+    player2=document.getElementById("player2").Value;
+    localStorage.setItem("player1", player1);
+    localStorage.setItem("player2", player2);
+    window.location="game.html";
+}
